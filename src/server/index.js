833a633fffe32f66a849b71cf6812967971895bd
TheCatsPajammies <67980898+TheCatsPajammies@users.noreply.github.com>
2021-01-13 21:21:21 -0600

    Building server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,24 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const fetch = require("node-fetch");
+const path = require('path')
+
+
+const cors = require('cors')
+app.use(cors())
+
 const dotenv = require('dotenv');
 dotenv.config();
 const API_KEY = process.env.API_KEY;
-const API_URL = process.env.API_URL;
+const BASE_URL = 'https://api.meaningcloud.com/sentiment-2.1?';
+
+// API_URL = `${BASE_URL}key=${API_KEY}&url=${userInput}`
 
 const app = express()
 
@@ -19,10 +33,12 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+app.post('')
