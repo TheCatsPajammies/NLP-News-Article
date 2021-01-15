2604fce7ccdbde2c476ffbb1811813e99be00e54
TheCatsPajammies <67980898+TheCatsPajammies@users.noreply.github.com>
2021-01-15 16:38:35 -0600

    Changes to paths

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -9,16 +9,16 @@ function handleSubmit(event) {
     console.log("::: Form Submitted :::")
 
     postData('http://localhost:8081/api', {url:formTest})
-        .then(res => {
+    .then(res => {
         document.getElementById('polarity').innerHTML = res.score_tag;
         document.getElementById("agreement").innerHTML = res.agreement;
         document.getElementById("subjectivity").innerHTML = res.subjectivity;
         document.getElementById("confidence").innerHTML = res.confidence;
         document.getElementById("irony").innerHTML = res.irony;
         })
-        } else {
-            alert('Please enter a valid URL');
-        }
+    } else {
+        alert('Please enter a valid URL');
+    }
 }
 
 const postData = async (url = "", data = {}) => {
