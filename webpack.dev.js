e615db28bd205ac7ce341a2716b7e256821ab05c
TheCatsPajammies <67980898+TheCatsPajammies@users.noreply.github.com>
2021-01-15 18:48:24 -0600

    Updating/building

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,12 +8,30 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+        path: path.resolve(__dirname, './dist'),
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            }
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
